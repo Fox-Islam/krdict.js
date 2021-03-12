264fd500835a77dd9ae801b37896e36cfec10ead
Fox Islam <foxislam@outlook.com>
2021-03-12 20:28:15 +0000

    1.4.0

--- src/value_types/vocabulary_grade.ts
+++ src/value_types/vocabulary_grade.ts
@@ -1,21 +1,14 @@
 const mapKey = {
     all: 'all',
-	level1: 'level1',
-	level2: 'level2',
-	level3: 'level3',
-	beginner: 'level1',
-	intermediate: 'level2',
-	advanced: 'level3',
+    level1: 'level1',
+    level2: 'level2',
+    level3: 'level3',
+    beginner: 'level1',
+    intermediate: 'level2',
+    advanced: 'level3',
 };
 
-type VocabularyGrade =
-	| 'all'
-	| 'level1'
-	| 'level2'
-	| 'level3'
-	| 'beginner'
-	| 'intermediate'
-	| 'advanced';
+type VocabularyGrade = 'all' | 'level1' | 'level2' | 'level3' | 'beginner' | 'intermediate' | 'advanced';
 
 function mapVocabularyGrade(input: VocabularyGrade[]): string[] {
     return input.map((grade) => {
